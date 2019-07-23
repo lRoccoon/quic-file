33905fd2b2504c5ccc2c6787bdf7d53b66fd2a24
yundongxing <18253016092@163.com>
2019-07-23 16:37:32 +0800

    upload and download

--- main.go
+++ main.go
@@ -11,16 +11,31 @@ import (
 func main() {
 	isServer := flag.Bool("s", false, "server mode")
 	isClient := flag.Bool("c", false, "client mode")
+
+	isUpload := flag.Bool("u", false, "upload file")
+	isDownload := flag.Bool("d", false, "download file")
 	flag.Parse()
 
 	if (*isServer && *isClient) || (!*isServer && !*isClient) {
 		log.Fatalln("server or client?")
 	}
-	if *isServer {
-		server.Server("[::]:8000")
+	if (*isUpload && *isDownload) || (!*isUpload && !*isDownload) {
+		log.Fatalln("upload or download?")
 	}
-	if *isClient {
-		client.Client("127.0.0.1:8000", "send.bin")
+	if *isUpload {
+		if *isServer {
+			server.Server("[::]:8000", "uploads.txt", true)
+		}
+		if *isClient {
+			client.Client("127.0.0.1:8000", "upload.txt", true)
+		}
 	}
-
+	if *isDownload {
+		if *isServer {
+			server.Server("[::]:8000", "download.txt", false)
+		}
+		if *isClient {
+			client.Client("127.0.0.1:8000", "downloads.txt", false)
+		}
+	}
 }
